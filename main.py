ddc997cdbe62ef5e4bfa92278b93fc9bebe5c71d
fred <domaine75@gmail.com>
2024-04-19 10:11:25 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-num = 3321321
+number = 3321321
 
 # To take input from the user
-# num = int(input("Enter a number: "))
+# number = int(input("Enter a number: "))
 
-if num == 1:
-    print(num, "is not a prime number")
-elif num > 1:
+if number == 1:
+    print(number, "is not a prime number")
+elif number > 1:
     # check for factors
-    for i in range(2, num):
-        if (num % i) == 0:
-            print(num, "is not a prime number")
-            print(i, "times", num // i, "is", num)
+    for i in range(2, number):
+        if (number % i) == 0:
+            print(number, "is not a prime number")
+            print(i, "times", number // i, "is", number)
             break
     else:
-        print(num, "is a prime number")
+        print(number, "is a prime number")
 
 # if input number is less than
 # or equal to 1, it is not prime
 else:
-    print(num, "is not a prime number")
+    print(number, "is not a prime number")
