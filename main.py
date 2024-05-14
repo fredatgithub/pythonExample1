c0624f796db8bc98e7b2916153bcd7207d31477e
fred <domaine75@gmail.com>
2024-05-14 16:29:04 +0200

    ajout liste de différence et écriture dans un fichier

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from Utils import IsPrimeByFred
 from Utils import IsPrimeByChatGPT
 
@@ -10,16 +12,20 @@ else:
     print(f'{number} is not prime')
 
 #for number in range(1000000,1000990):
-intMaxSize = 9223372036854775807
+#intMaxSize = 9223372036854775807
+intMaxSize = 100000000
 compteur = 0
 lastdifference = 0
 lastPrime = 2
+liste_de_difference = []
+#liste_de_chaines.append("Première chaîne")
 for number in range(2,intMaxSize):
     if IsPrimeByChatGPT(number):
         #print(f'{number}', end=" ")
         lastdifference = number - lastPrime
         lastPrime = number
-        print(f'{number} différence = {lastdifference}', end=" ")
+        print(f'{number} différence = {lastdifference}')
+        liste_de_difference.append(lastdifference)
         compteur += 1
 print('are prime numbers')
 
@@ -27,3 +33,12 @@ import sys
 
 #La taille maximale d'un entier sur cette plateforme est : 9_223_372_036_854_775_807 ou 9223372036854775807
 print("La taille maximale d'un entier sur cette plateforme est :", sys.maxsize)
+
+# Ouvrir un fichier en mode écriture
+with open('PrimeDifference.txt', 'w') as fichier:
+    # Écrire dans le fichier
+    #fichier.write("Bonjour, monde !\n")
+    for element in liste_de_difference:
+        fichier.write(str(element) + "\n")
+    
+print('Fichier écrit')
