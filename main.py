4cf7e5d04b736faba722174a29061f6ae52bdd2d
fred <domaine75@gmail.com>
2024-05-14 15:05:00 +0200

    add another prime method

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Utils import IsPrimeByFred
+from Utils import IsPrimeByChatGPT
 
 #Exemple utilisation
 number = 3321321
@@ -7,3 +8,7 @@ if result:
     print(f'{number} is prime')
 else:
     print(f'{number} is not prime')
+
+for number in range(1000000,1000090):
+    if IsPrimeByChatGPT(number):
+        print(f'{number} is prime')
