e312a7f252f823cbbccdfc3838b161771b3ad084
fred <domaine75@gmail.com>
2024-08-08 15:12:05 +0200

    clean code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from Utils import IsPrimeByFred
 from Utils import IsPrimeByChatGPT
 
-#Exemple utilisation
+# Exemple utilisation
 number = 3321321
 result = IsPrimeByFred(number)
 if result:
@@ -11,26 +11,26 @@ if result:
 else:
     print(f'{number} is not prime')
 
-#for number in range(1000000,1000990):
-#intMaxSize = 9223372036854775807
+# for number in range(1000000,1000990):
+# intMaxSize = 9223372036854775807
 intMaxSize = 500000000
 compteur = 0
-lastdifference = 0
+lastDifference = 0
 lastPrime = 2
 liste_de_difference = []
 line = ''
 for number in range(2, intMaxSize):
     if IsPrimeByChatGPT(number):
-        #print(f'{number}', end=" ")
-        lastdifference = number - lastPrime
+        # print(f'{number}', end=" ")
+        lastDifference = number - lastPrime
         lastPrime = number
-        print(f'{number} différence = {lastdifference}')
-        line = f'{number} - {lastdifference}'
+        print(f'{number} différence = {lastDifference}')
+        line = f'{number} - {lastDifference}'
         liste_de_difference.append(line)
         compteur += 1
 print('are prime numbers')
 
-#La taille maximale d'un entier sur cette plateforme est : 9_223_372_036_854_775_807 ou 9223372036854775807
+# La taille maximale d'un entier sur cette plateforme est : 9_223_372_036_854_775_807 ou 9223372036854775807
 print("La taille maximale d'un entier sur cette plateforme est :", sys.maxsize)
 
 # Ouvrir un fichier en mode écriture
